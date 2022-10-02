c21fc62903d2ca049845536cd05fabf8e2d6eae7
Biademery <biademery@gmail.com>
2022-10-02 19:21:36 -0300

    add calculator logic

--- djangoapi/views.py
+++ djangoapi/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 
 def index(request):
-    result = "This would be my result"
-    return HttpResponse(result)
+    hypotenuse = 5
+    opposite = ""
+    adjacent = 3
+
+    if hypotenuse == "":
+        hypotenuse = (opposite * opposite + adjacent * adjacent) ** (1 / 2)
+        return HttpResponse(hypotenuse)
+
+    elif opposite == "":
+        opposite = (hypotenuse * hypotenuse - adjacent * adjacent) ** (1 / 2)
+        return HttpResponse(opposite)
+
+    elif adjacent == "":
+        adjacent = (hypotenuse * hypotenuse - opposite * opposite) ** (1 / 2)
+        return HttpResponse(adjacent)
