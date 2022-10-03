36d5c27bc07edec5a5390ec202c5e6bd77cea209
Biademery <biademery@gmail.com>
2022-10-03 16:31:53 -0300

    limit float response to two decimal

--- djangoapi/views.py
+++ djangoapi/views.py
@@ -13,15 +13,15 @@ def index(request):
 
     if not hypotenuse and opposite and adjacent:
         hypotenuse = ((opposite ** 2) + (adjacent ** 2)) ** 0.5
-        return HttpResponse(hypotenuse)
+        return HttpResponse("{:.2f}".format(hypotenuse))
 
     elif not opposite and hypotenuse and adjacent:
         opposite = ((hypotenuse ** 2) - (adjacent ** 2)) ** 0.5
-        return HttpResponse(opposite)
+        return HttpResponse("{:.2f}".format(opposite))
 
     elif not adjacent and hypotenuse and opposite:
         adjacent = ((hypotenuse ** 2) - (opposite ** 2)) ** 0.5
-        return HttpResponse(adjacent)
+        return HttpResponse("{:.2f}".format(adjacent))
 
     else: 
         return HttpResponseBadRequest()
